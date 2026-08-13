a366ae003af4b8733584a674d04b6e1cd23a5fa8
mani-vijay-apps <replyto.manikandan@gmail.com>
2026-08-13 18:04:47 +0530

    chore: Initialized the fastify typescript node js backend server

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import Fastify from 'fastify';
+
+const app = Fastify({
+    logger:true,
+});
+
+// GET health
+app.get('/health', async ()=>{
+    return {status:"ok"};
+});
+
+const start = async () => {
+  try{
+    await app.listen({
+        port:8080,
+        host: "0.0.0.0"
+    });
+  }catch(err){
+    app.log.error(err);
+    process.exit(1);
+  }   
+};
+
+start();
